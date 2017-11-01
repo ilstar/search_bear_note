2df6bac6a510d8e0e62c6972be2d5ca954830428
Fred Liang <fred@instacart.com>
2017-11-01 12:18:26 -0700

    Fix test

--- bear_helper_test.rb
+++ bear_helper_test.rb
@@ -6,12 +6,21 @@ class TestBearHelper < MiniTest::Unit::TestCase
     # p BearHelper::ScriptFilter.new('').send :get_top_10
   end
 
-  def test_perform
-    # puts BearHelper::ScriptFilter.new('insta').perform
+  def test_perform_when_query_is_empty_string
+    filter = BearHelper::ScriptFilter.new("")
+    filter.stub(:history_content, "pizza\nbamboo") do
+      assert filter.perform == %{<xml><items><item arg="pizza"><title>pizza</title></item><item arg="bamboo"><title>bamboo</title></item></items></xml>}
+    end
+  end
+
+  def test_perform_when_query_has_something
+    filter = BearHelper::ScriptFilter.new("bam")
+    filter.stub(:history_content, "pizza\nbamboo") do
+      assert filter.perform == %{<xml><items><item arg="bam"><title>bam</title></item><item arg="bamboo"><title>bamboo</title></item></items></xml>}
+    end
   end
 
   def test_callback_url_generator
-    # puts BearHelper::CallbackUrlGenerator.new('abc').perform
     assert BearHelper::CallbackUrlGenerator.new('abc').perform == "bear://x-callback-url/search?term=abc"
     assert BearHelper::CallbackUrlGenerator.new('#abc').perform == "bear://x-callback-url/search?tag=abc"
   end
